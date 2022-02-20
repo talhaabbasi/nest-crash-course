64bbfe1d1052bdbb863c73f86e2c73e87d3db8ff
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-21 00:16:19 +0500

    [FEAT] Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { StudentController } from 'src/student/student.controller';
-import { StudentService } from 'src/student/student.service';
-import { StudentTeacherController } from 'src/teacher/student.controller';
-import { TeacherController } from 'src/teacher/teacher.controller';
+import { StudentModule } from 'src/student/student.module';
 
 @Module({
-  imports: [],
-  controllers: [StudentController, TeacherController, StudentTeacherController],
-  providers: [StudentService],
+  imports: [StudentModule],
 })
 export class AppModule {}
