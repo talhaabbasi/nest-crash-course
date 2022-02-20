f22bef3b954597869477544165a35ab04b77244c
talhaabbasi <talhaabbasi1997@gmail.com>
2022-02-20 19:34:43 +0500

    [FEAT] Add request objects

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('student')
 export class StudentController {
@@ -8,7 +8,21 @@ export class StudentController {
   }
 
   @Get('/:studentId')
-  getStudentsById() {
-    return 'Get Student By Id';
+  getStudentsById(@Param('studentId') studentId: string) {
+    console.log(studentId);
+    return `Get Student with id ${studentId} `;
+  }
+
+  @Post()
+  createStudent(@Body() body) {
+    console.log(body);
+    return 'Create Student';
+  }
+
+  @Put()
+  updateStudent(@Param('studentId') studentId: string, @Body() body) {
+    return `Update Student with Id of ${studentId} with Data of ${JSON.stringify(
+      body,
+    )}`;
   }
 }
